391dbfc916ff274f0cb77925d81a610c91f099fe
Cramessar <154475437+Cramessar@users.noreply.github.com>
2025-04-06 19:42:00 -0400

    adding comments for clarity

--- seed_data.py
+++ seed_data.py
@@ -1,4 +1,7 @@
 # File: seed_data.py
+# Run this file to populate or clean db to default state.
+# seed.py shows all the info that will populate.
+
 from flask_app import app
 from seed import seed_db
 
